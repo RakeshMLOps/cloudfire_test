02b0dc753f2743f1eb1f289961d3d1ff143b26b6
RakeshMLOps <165798206+RakeshMLOps@users.noreply.github.com>
2025-04-17 09:50:09 +0530

    Updated file with required git release notes for app.java

--- app.java
+++ app.java
@@ -1,2 +1,9 @@
 Hello all 
 Welcome to RR Tecchnosoft
+
+New Features
+Comes with Git v2.49.0.
+Comes with OpenSSH v9.9.P2.
+Comes with PCRE2 v10.45.
+The previously-experimental --full-name-hash option has been accepted into upstream Git as --name-hash-version=2 and is no longer experimental.
+The git backfill command has been accepted into upstream Git; Its --batch-size=<n> option has been renamed to --min-batch-size=<n>, though.

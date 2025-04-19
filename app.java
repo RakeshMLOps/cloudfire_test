5091db810212d6dd268ec9a5c39328c56e26f08d
DevopsguruRR <rakeshdevopstrainer1@gmail.com>
2025-04-19 09:48:48 +0530

    fixing code error as per request

--- app.java
+++ app.java
@@ -5,8 +5,17 @@ New Features
 Comes with Git v2.49.0.
 Comes with OpenSSH v9.9.P2.
 Comes with PCRE2 v10.45.
-The previously-experimental --full-name-hash option has been accepted into upstream Git as --name-hash-version=2 and is no longer experimental.
-The git backfill command has been accepted into upstream Git; Its --batch-size=<n> option has been renamed to --min-batch-size=<n>, though.
 
 
 As per adhi request modifying file to understand for timestamp in git central repo
+
+git branch
+list your branches. a * will appear next to the currently active branch
+git branch [branch-name]
+create a new branch at the current commit
+git checkout
+switch to another branch and check it out into your working directory
+git merge [branch]
+merge the specified branch’s history into the current one
+git log
+show all commits in the current branch’s history
